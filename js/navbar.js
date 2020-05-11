90c6fa7e51af059af204e25b5ebf6653c10d2fcc
DzambaTz <tarik.dzambic@gmail.com>
2020-05-11 02:11:37 +0200

    Dodani sidebars, dvije nove sekcije, i natpis u headeru. WIP

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+function openNav(navName) {
+  if (navName == "leftSidenav")
+    document.getElementById(navName).style.width = "100%";
+  else {
+    document.getElementById(navName).style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+}
+
+function closeNav(navName) {
+  if (navName == "leftSidenav")
+    document.getElementById(navName).style.width = "0";
+  else {
+    document.getElementById(navName).style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+}
