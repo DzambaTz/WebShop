716f7d6ead201f2f5ae0a828790cf81edc6675c1
DzambaTz <tarik.dzambic@gmail.com>
2020-05-17 01:55:25 +0200

    Kontakt stranica zavrsena, izmjene navbara u proizvodi.html

---
+++ js/updown.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#pitanje1").click(function () {
+    $("#odg1").slideToggle("slow");
+    $(".fa-chevrown");
+  });
+  $("#pitanje2").click(function () {
+    $("#odg2").slideToggle("slow");
+  });
+  $("#pitanje3").click(function () {
+    $("#odg3").slideToggle("slow");
+  });
+  $("#pitanje4").click(function () {
+    $("#odg4").slideToggle("slow");
+  });
+});
