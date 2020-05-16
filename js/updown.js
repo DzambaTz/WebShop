22caa770acd60e41a25a0d8a4560a7dd1c2af662
NedimDzelilovic <nedimdzelilovic5@gmail.com>
2020-05-16 16:45:09 +0200

    dodao sam jos jedan div u kontakt

---
+++ js/updown.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+  $("#pitanje1").click(function(){
+    $("#odg1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#pitanje2").click(function(){
+    $("#odg2").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#pitanje3").click(function(){
+    $("#odg3").slideToggle("slow");
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#pitanje4").click(function(){
+    $("#odg4").slideToggle("slow");
+  });
+});
